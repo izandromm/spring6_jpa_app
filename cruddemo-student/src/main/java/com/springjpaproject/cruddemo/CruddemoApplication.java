20b222efc5cac7246a64f380aeee677a496728c5
Izandro <izandromm@gmail.com>
2023-11-30 21:53:04 -0300

    runningn student saving methods.

--- cruddemo-student/src/main/java/com/springjpaproject/cruddemo/CruddemoApplication.java
+++ cruddemo-student/src/main/java/com/springjpaproject/cruddemo/CruddemoApplication.java
@@ -1,5 +1,7 @@
 package com.springjpaproject.cruddemo;
 
+import com.springjpaproject.cruddemo.dao.StudentDAO;
+import com.springjpaproject.cruddemo.entity.Student;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,11 +15,28 @@ public class CruddemoApplication {
 	}
 
 	@Bean
-	public CommandLineRunner commandLineRunner(String[] args){
+	public CommandLineRunner commandLineRunner(StudentDAO studentDAO){
 
 		return runner -> {
-			System.out.println("Hello word");
+			createStudent(studentDAO);
 		};
 	}
 
+	private void createStudent(StudentDAO studentDAO) {
+		// create the student object
+		System.out.println("Creating new student object ...");
+		Student tempStudent = new Student("Paul", "Doe","paul@studentcode.com");
+
+		// save the student object
+		System.out.println("Saving the student ...");
+		studentDAO.save(tempStudent);
+
+		// display id of the saved student
+		System.out.println("Saved student. Generated id: " + tempStudent.getId());
+	}
+
 }
+
+
+
+
