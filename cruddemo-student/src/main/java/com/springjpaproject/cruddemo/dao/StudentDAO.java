ae64504899d44fb856f5414eb5c84057cca15d7b
Izandro <izandromm@gmail.com>
2023-12-03 18:46:53 -0300

    created basic crud cli application.

--- cruddemo-student/src/main/java/com/springjpaproject/cruddemo/dao/StudentDAO.java
+++ cruddemo-student/src/main/java/com/springjpaproject/cruddemo/dao/StudentDAO.java
@@ -2,8 +2,22 @@ package com.springjpaproject.cruddemo.dao;
 
 import com.springjpaproject.cruddemo.entity.Student;
 
+import java.util.List;
+
 public interface StudentDAO {
 
     void save(Student theStudent);
+
+    Student findById(Integer id);
+
+    List<Student> findAll();
+
+    List<Student> findByLastName(String theLastName);
+
+    void update(Student theStudent);
+
+    void delete(Integer id);
+
+    int deleteAll();
 
 }
